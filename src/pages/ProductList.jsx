9c8d8a32a9d3dd78bdd5906bfd35cfaa21472842
Jaqueline Lin <XJACKIECO@gmail.com>
2022-05-02 21:43:23 -0400

    Update ProductList.jsx

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,12 +1,23 @@
 import styled from "styled-components"
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement"
 
-const Container = styled.div`
-
-`
+const Container = styled.div``
+const Title = styled.h1``
+const FilterContainer = styled.div``
+const Filter = styled.div``
 
 export const ProductList = () => {
   return (
-    <div>ProductList insert here</div>
+    <Container>
+      <Navbar/>
+      <Announcement/>
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter>Filter 1</Filter>
+        <Filter>Filter 2</Filter>
+      </FilterContainer>
+    </Container>
   )
 }
 
