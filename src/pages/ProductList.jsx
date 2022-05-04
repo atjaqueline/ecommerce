a70ccd5ff4b1722bd60f3e473dc2e27b29d2a688
Jaqueline Lin <XJACKIECO@gmail.com>
2022-05-03 20:35:36 -0400

    Update ProductList.jsx

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,11 +1,26 @@
 import styled from "styled-components"
 import Navbar from "../components/Navbar";
 import Announcement from "../components/Announcement"
+import Products from "../components/Products"
+import Newsletter from "../components/Newsletter"
+import Footer from "../components/Footer"
 
 const Container = styled.div``
-const Title = styled.h1``
-const FilterContainer = styled.div``
-const Filter = styled.div``
+
+const Title = styled.h1`
+margin: 20px;`
+
+const FilterContainer = styled.div`
+display: flex;
+justify-content: space-between;`
+
+const Filter = styled.div`
+margin: 20px;`
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+`
 
 export const ProductList = () => {
   return (
@@ -14,9 +29,12 @@ export const ProductList = () => {
       <Announcement/>
       <Title>Dresses</Title>
       <FilterContainer>
-        <Filter>Filter 1</Filter>
-        <Filter>Filter 2</Filter>
+        <Filter><FilterText>Filter Products</FilterText></Filter>
+        <Filter><FilterText>Sort Products</FilterText></Filter>
       </FilterContainer>
+      <Products/>
+      <Newsletter/>
+      <Footer/>
     </Container>
   )
 }
